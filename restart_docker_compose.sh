ace164ffefea5a0adc569aa582e6e59e5abfdcea
Thomas Jeong <unistommy@gmail.com>
2021-11-14 17:43:51 +0900

    Client Server 2 Added

--- restart_docker_compose.sh
+++ restart_docker_compose.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 docker-compose down
-docker rmi nfs_project_nfs_server nfs_project_client_server
+docker rmi nfs_project_nfs_server nfs_project_client_server1 nfs_project_client_server2
 docker network rm nfs_project_nfs_server_network
 docker-compose up
